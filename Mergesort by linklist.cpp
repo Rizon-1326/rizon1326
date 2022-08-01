4a476f5f96b086e630e4532018a0e365b3d4dd9b
rizon1326 <109311760+rizon1326@users.noreply.github.com>
2022-08-01 20:44:27 +0600

    Update Mergesort by linklist.cpp

--- Mergesort by linklist.cpp
+++ Mergesort by linklist.cpp
@@ -1,110 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Link list node */
 struct Node {
-	int data;
-	struct Node* next;
+    int data;
+    struct Node* next;
 };
 
+/* function prototypes */
 struct Node* SortedMerge(struct Node* a, struct Node* b);
-void FrontBackSplit(struct Node* source,
-					struct Node** frontRef, struct Node** backRef);
+void middle(struct Node* source,
+                    struct Node** frontRef, struct Node** backRef);
 
-void MergeSort(struct Node** headRef)
-{
-	struct Node* head = *headRef;
-	struct Node* a;
-	struct Node* b;
+/* sorts the linked list by changing links */
+void MergeSort(struct Node** headRef){
+    struct Node* head = *headRef;
+    struct Node* a;
+    struct Node* b;
 
-	if ((head == NULL) || (head->next == NULL)) {
-		return;
-	}
-	FrontBackSplit(head, &a, &b);
+    /* Base case -- length 0 or 1 */
+    if ((head == NULL) || (head->next == NULL)) {
+        return;
+    }
 
-	MergeSort(&a);
-	MergeSort(&b);
-	*headRef = SortedMerge(a, b);
+    middle(head, &a, &b);
+
+    MergeSort(&a);
+    MergeSort(&b);
+
+    *headRef = SortedMerge(a, b);
 }
 
 struct Node* SortedMerge(struct Node* a, struct Node* b)
 {
-	struct Node* result = NULL;
+    struct Node* result = NULL;
 
-	/* Base cases */
-	if (a == NULL)
-		return (b);
-	else if (b == NULL)
-		return (a);
-  
-	if (a->data <= b->data) {
-		result = a;
-		result->next = SortedMerge(a->next, b);
-	}
-	else {
-		result = b;
-		result->next = SortedMerge(a, b->next);
-	}
-	return (result);
+    /* Base cases */
+    if (a == NULL)
+        return (b);
+    else if (b == NULL)
+        return (a);
+
+    if (a->data <= b->data) {
+        result = a;
+        result->next = SortedMerge(a->next, b);
+    }
+    else {
+        result = b;
+        result->next = SortedMerge(a, b->next);
+    }
+    return (result);
 }
 
-void FrontBackSplit(struct Node* source,
-					struct Node** frontRef, struct Node** backRef)
+/* This function splits the list from the middle */
+/* After splitting Original list converts into two list 
+   first list -> head to the node previous of middle and second list -> middle to end */
+
+void middle(struct Node* source,
+                    struct Node** frontRef, struct Node** backRef)
 {
-	struct Node* fast;
-	struct Node* slow;
-	slow = source;
-	fast = source->next;
+    struct Node* fast;
+    struct Node* slow;
+    slow = source;
+    fast = source->next;
 
-	while (fast != NULL) {
-		fast = fast->next;
-		if (fast != NULL) {
-			slow = slow->next;
-			fast = fast->next;
-		}
-	}
+    /* Advance 'fast' two nodes, and advance 'slow' one node */
+    while (fast != NULL) {
+        fast = fast->next;
+        if (fast != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+    }
 
-	*frontRef = source;
-	*backRef = slow->next;
-	slow->next = NULL;
+    /* 'slow' is before the midpoint in the list, so split it in two
+    at that point. */
+    *frontRef = source;
+    *backRef = slow->next;
+    slow->next = NULL;
 }
 
-void printList(struct Node* node)
-{
-	while (node != NULL) {
-		printf("%d ", node->data);
-		node = node->next;
-	}
+/* Function to print linked list */
+void printList(struct Node* node){
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
 }
-void push(struct Node** head_ref, int new_data)
-{
-	
-	struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
 
-	new_node->data = new_data;
+/* Function to insert a node at the beginning of the list */
+void push(struct Node** head_ref, int new_data){
+    /* allocate node */
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
 
-	new_node->next = (*head_ref);
+    /* assign the data */
+    new_node->data = new_data;
 
-	(*head_ref) = new_node;
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);
+
+    /* move the head pointer to point to the new node */
+    (*head_ref) = new_node;
 }
-int main()
-{
-	/* Start with the empty list */
-	struct Node* res = NULL;
-	struct Node* a = NULL;
 
-//Created lists shall be a: 2->3->20->5->10->15 
-	push(&a, 15);
-	push(&a, 10);
-	push(&a, 5);
-	push(&a, 20);
-	push(&a, 3);
-	push(&a, 2);
+int main(){
+    
+    struct Node* res = NULL;
+    struct Node* a = NULL;
 
-	/* Sort the above created Linked List */
-	MergeSort(&a);
+    /* Let's create a new linked lists */
+   /* a: 8->9->5->3->2 */
+    push(&a, 2);
+    push(&a, 3);
+    push(&a, 5);
+    push(&a, 9);
+    push(&a, 8);
+    
+    printf("Linked List before sorting: \n");
+    printList(a);
+    printf("\n");
+    /* Sort the Linked List */
+    MergeSort(&a);
+    printf("Linked List after Sorting: \n");
+    printList(a);
 
-	printf("Sorted Linked List is: \n");
-	printList(a);
-
-	getchar();
-	return 0;
+    getchar();
+    return 0;
 }
+</stdlib.h></stdio.h>
